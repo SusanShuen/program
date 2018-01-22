54f364d5c71a33a94e9735a51692116b1ea36e78
SusanShuen <ys1811@outlook.com>
2018-01-22 10:58:07 +0000

    Add files via upload

---
+++ pyoptflow-master-copy/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+req = ['nose','setuptools','pillow','scipy','numpy',
+       'future-fstrings']
+preq = ['matplotlib']
+# %%
+from setuptools import setup,find_packages
+
+setup(name='pyoptflow',
+      packages=find_packages(),
+      author='Michael Hirsch, Ph.D.',
+      url='https://github.com/scivision/pyoptflow',
+      version='1.1.0',
+      classifiers=[
+      'Topic :: Scientific/Engineering',
+      'Development Status :: 3 - Alpha',
+      'Programming Language :: Python :: 3',
+      ],
+      python_requires = '>=3.5',
+      install_requires = req,
+      extras_require={'plot':preq},
+      description='Pure Python optical flow: Horn-Schunck, Lucas-Kanade',
+	  )
